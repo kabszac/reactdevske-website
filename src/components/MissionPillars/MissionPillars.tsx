0d4351c6986e87f18cfccfa3546b75d27b674cfb
Isaac Kabucho <kabszac@gmail.com>
2023-04-09 20:50:42 +0300

    added cdn image on 1st image on mission pillars

--- src/components/MissionPillars/MissionPillars.tsx
+++ src/components/MissionPillars/MissionPillars.tsx
@@ -15,7 +15,7 @@ export default function MissionPillars() {
           <div className="flex w-1/2 md:w-full">
             <div className="w-full overflow-hidden aspect-w-4 aspect-h-3">
               <Image
-                src={'/images/mentorship-image.jpg'}
+                src='https://res.cloudinary.com/reactjs-devs-ke/image/upload/v1678532588/website-images/mentorship-image_tz0mor.jpg'
                 alt="Mentorship Image"
                 layout="fill"
               />
